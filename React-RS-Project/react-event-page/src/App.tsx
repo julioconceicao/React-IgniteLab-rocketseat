4c94d3229ca8f47b6545a9c608f802a1eb11ca59
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-22 23:23:33 -0300

    :bricks: ci: App, main and colors changes

--- React-RS-Project/react-event-page/src/App.tsx
+++ React-RS-Project/react-event-page/src/App.tsx
@@ -1,26 +1,12 @@
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
+import React from "react";
+import { Event } from "./pages/Event";
 
 function App() {
 
-  const GET_LESSONS_QUERY = gql`
-    query{
-      lessons {
-        id
-        title
-      }
-    }
-    `
-
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }) .then(response => {
-      console.log(response.data)
-    })
-  }, [])
   return (
-   <h1 className="text-violet-400 text-5xl font-bold">Hello World</h1>
+    <div>
+      <Event />
+    </div>
   )
 }
 
