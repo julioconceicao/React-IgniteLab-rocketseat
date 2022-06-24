da394d0a1ba7e46d361eb2dfae201e8282c554b2
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-23 23:37:23 -0300

    :bricks: ci: Structuring Event Page v1.2

--- React-RS-Project/react-event-page/src/pages/Event.tsx
+++ React-RS-Project/react-event-page/src/pages/Event.tsx
@@ -1,9 +1,16 @@
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+import { Video } from "../components/Video";
 
-export function Event(){
-    return(
+export function Event() {
+    return (
+        <div className="flex flex-col min-h-screen">
 
-        <Header />
-
-        )
+            <Header />
+            <main className="flex">
+                <Video />
+                <Sidebar />
+            </main>
+        </div>
+    )
 }
