2924337c801da11b82a5fb6205451446c8b083bc
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-06-24 19:18:38 -0300

    :bricks: ci: Structuring Event Page + add Router

--- React-RS-Project/react-event-page/src/App.tsx
+++ React-RS-Project/react-event-page/src/App.tsx
@@ -1,11 +1,19 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
+import { client } from "./lib/apollo";
 import { Event } from "./pages/Event";
 
 function App() {
 
   return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   )
 }
--- React-RS-Project/react-event-page/src/pages/Event.tsx
+++ React-RS-Project/react-event-page/src/pages/Event.tsx
@@ -1,14 +1,23 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
+
 export function Event() {
+    const { slug } = useParams<{ slug: string }>()
     return (
         <div className="flex flex-col min-h-screen">
 
             <Header />
+
             <main className="flex">
-                <Video />
+                { slug
+                    ? <Video lessonSlug={slug}/>
+                    : <div className="flex-1 justify-center items-center">
+                        <h1 className="justify-center text-orange-500 font-bold">Selecione uma aula</h1>
+                    </div>
+                }
                 <Sidebar />
             </main>
         </div>
